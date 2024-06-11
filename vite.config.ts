4af5147ecf11962e0f977547440890262e8f132f
Shayan bista <shayanbista94@gmail.com>
2024-06-11 06:07:06 +0545

    deployment try

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/lf-fellow-assignment-5-image-carousel",
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
